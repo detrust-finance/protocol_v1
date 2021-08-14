be6ed45c24db775bf1347aabb0861ea86345e3ea
Web Ninja <x7y8tt@gmail.com>
2021-08-14 15:43:42 +0800

    Print the logic contract address

--- scripts/deploy-proxy.js
+++ scripts/deploy-proxy.js
@@ -4,7 +4,16 @@ async function main() {
   console.log("Deploying ...");
   const deTrust = await upgrades.deployProxy(DeTrust);
   await deTrust.deployed();
-  console.log("Deployed to:", deTrust.address);
+  console.log("Deployed to\n  proxy: ", deTrust.address);
+
+  // [Expose implementation address in hardhat plugin](https://github.com/OpenZeppelin/openzeppelin-upgrades/issues/313)
+  // https://eips.ethereum.org/EIPS/eip-1967#logic-contract-address
+  const implHex = await ethers.provider.getStorageAt(
+    deTrust.address,
+    "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
+  );
+  const logicAddress = ethers.utils.hexStripZeros(implHex);
+  console.log("  logic: ", logicAddress);
 }
 
 main()
