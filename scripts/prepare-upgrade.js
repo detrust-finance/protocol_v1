5510d512ab3a0ee5141e217f1f61c2e0121317b5
onigirisan <onigirisan.chen@gmail.com>
2021-09-16 16:35:52 +0800

    update deploy scripts & deploy to rinkeby

--- scripts/prepare-upgrade.js
+++ scripts/prepare-upgrade.js
@@ -1,11 +1,46 @@
+const { loadPreviousDeployment, verifyContract } = require("./utils.js");
+const { ethers, upgrades, network } = require("hardhat");
+const argumentsArray = require("./timelock_arguments.js");
+const UPGRADE_ABI = [
+  "function upgrade(address proxy, address implementation)",
+  "function schedule(address target, uint256 value, bytes calldata data, bytes32 predecessor, bytes32 salt, uint256 delay)",
+  "function execute(address target, uint256 value, bytes calldata data, bytes32 predecessor, bytes32 salt)",
+];
+const iface = new ethers.utils.Interface(UPGRADE_ABI);
 async function main() {
-  const proxyAddress = "0x719A8D43aE6418D6ea87E84BC6Cb3d2DEcA0BC91"; // This is the proxy address NOT the admin of the proxy.
-
+  const deploymentState = loadPreviousDeployment(network.name);
   const DeTrust = await ethers.getContractFactory("DeTrust");
   console.log("Preparing upgrade...");
-  const boxV2Address = await upgrades.prepareUpgrade(proxyAddress, BoxV2);
-  const deTrust = await upgrades.prepareUpgrade(proxyAddress, DeTrust);
+  const deTrust = await upgrades.prepareUpgrade(
+    deploymentState.DeTrust.address,
+    DeTrust
+  );
   console.log("upgraded at:", deTrust);
+  await verifyContract(deTrust);
+  const data = iface.encodeFunctionData("upgrade", [
+    deploymentState.DeTrust.address,
+    deTrust,
+  ]);
+  const proxy_admin = require(`../.openzeppelin/${network.name}.json`).admin
+    .address;
+  console.log("upgraded data:", data);
+  const schedule_data = iface.encodeFunctionData("schedule", [
+    proxy_admin,
+    0,
+    data,
+    "0x0000000000000000000000000000000000000000000000000000000000000000",
+    "0x0000000000000000000000000000000000000000000000000000000000000000",
+    argumentsArray[0],
+  ]);
+  console.log("schedule data:", schedule_data);
+  const execute_data = iface.encodeFunctionData("execute", [
+    proxy_admin,
+    0,
+    data,
+    "0x0000000000000000000000000000000000000000000000000000000000000000",
+    "0x0000000000000000000000000000000000000000000000000000000000000000",
+  ]);
+  console.log("execute data:", execute_data);
 }
 
 main()
---
+++ scripts/timelock_arguments.js
@@ -0,0 +1,5 @@
+module.exports = [
+  150, // minimum delay timer in seconds
+  ["0x475Bf356ED6Bd8FaCc13bA977cea27dbD6bc7289"], // Proposers Array
+  ["0x475Bf356ED6Bd8FaCc13bA977cea27dbD6bc7289"], // Executors Array
+];
